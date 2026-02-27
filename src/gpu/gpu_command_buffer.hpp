ae7066bdc2cb1493effa686537bf064e2e76ed05
RuiwenTang <tangruiwen1989@gmail.com>
2026-02-27 10:58:03 +0800

    chore(gpu): Remove unused resource proxy
    These proxy is no longer used in post process filter pass.

--- src/gpu/gpu_command_buffer.hpp
+++ src/gpu/gpu_command_buffer.hpp
@@ -33,39 +33,6 @@ class GPUCommandBuffer {
   std::string label_;
 };
 
-class GPUCommandBufferProxy : public GPUCommandBuffer {
- public:
-  explicit GPUCommandBufferProxy(
-      std::shared_ptr<GPUCommandBuffer> command_buffer)
-      : command_buffer_(command_buffer) {}
-
-  std::shared_ptr<GPURenderPass> BeginRenderPass(
-      const GPURenderPassDescriptor& desc) override {
-    auto render_pass = std::make_shared<GPURenderPassProxy>(desc);
-    render_passes_.push_back(render_pass);
-    return render_pass;
-  }
-
-  std::shared_ptr<GPUBlitPass> BeginBlitPass() override { return nullptr; }
-
-  bool Submit() override {
-    for (auto& render_pass_proxy : render_passes_) {
-      auto render_pass =
-          command_buffer_->BeginRenderPass(render_pass_proxy->GetDescriptor());
-      for (auto& command : render_pass_proxy->GetCommands()) {
-        render_pass->AddCommand(command);
-      }
-      render_pass->EncodeCommands(render_pass_proxy->viewport_,
-                                  render_pass_proxy->scissor_);
-    }
-    return command_buffer_->Submit();
-  }
-
- private:
-  std::vector<std::shared_ptr<GPURenderPassProxy>> render_passes_;
-  std::shared_ptr<GPUCommandBuffer> command_buffer_;
-};
-
 }  // namespace skity
 
 #endif  // SRC_GPU_GPU_COMMAND_BUFFER_HPP
